4e7fceac3c9249a93aadf82100921f41f08e5a34
Gabriel Coronel <gabriel.c1901@gmail.com>
2023-03-02 23:26:08 -0300

    add view friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: %i[create destroy]
   devise_for :users
-  root :to => 'welcome#index'
+  resources :stocks
+  resources :users, only: [:show]
+  resources :friendships
+
+  root to: 'welcome#index'
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
-  resources :stocks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
